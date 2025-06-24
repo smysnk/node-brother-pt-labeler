591c61e1a76b4563c8525f2b5c3ed6e3f641e37b
Sefa Ilkimen <silkimen@users.noreply.github.com>
2025-06-24 04:11:52 +0200

    chore: add TS types

--- src/brother.ts
+++ src/brother.ts
@@ -1,13 +1,166 @@
 import ipp from '@sealsystems/ipp';
-const util = require('util');
-const fs = require('fs');
-const pngparse = require('pngparse');
+import util from 'node:util';
+import pngparse from 'pngparse';
 
-export function wait(time) {
+const parseFile = util.promisify(pngparse.parseFile);
+const parse = util.promisify(pngparse.parse);
+
+export interface PrintOptions {
+  /** default: 128 */
+  blackwhiteThreshold?: number;
+  /** default: false */
+  highResolution?: boolean;
+  /** default: 12  */
+  tapeWidth?: 12 | 18 | 24;
+}
+
+type InternalOptions = Required<PrintOptions>;
+type ParsedImageData = ImageData & { channels: 1 | 2 | 3 | 4 };
+type PixelMatrix = { height: number, width: number, data: number[][] };
+
+function wait(time: number) {
   return new Promise(resolve => setTimeout(resolve, time));
 }
 
-export async function doPrint(printerUrl, bufferToBePrinted) {
+function convertToBlackAndWhiteMatrix(image: ParsedImageData, threshold: number): PixelMatrix {
+  const rows: number[][] = [];
+
+  for (let y = 0; y < image.height; y++) {
+    const cols: number[] = [];
+
+    for (let x = 0; x < image.width; x++) {
+      let pos = x + image.width * y;
+
+      pos = pos * image.channels;
+
+      // 1 channel : grayscale
+      // 2 channels: grayscale + alpha
+      // 3 channels: RGB
+      // 4 channels: RGBA
+      switch (image.channels) {
+        case 1:
+          cols.push(image.data[pos] < threshold ? 1 : 0);
+          break;
+
+        case 2:
+          cols.push((image.data[pos] * image.data[pos + 1] / 255) < threshold ? 1 : 0);
+          break;
+
+        case 3:
+          cols.push((0.21 * image.data[pos] + 0.72 * image.data[pos + 1] + 0.07 * image.data[pos + 2]) < threshold ? 1 : 0);
+          break;
+
+        case 4:
+          cols.push(((0.21 * image.data[pos] + 0.72 * image.data[pos + 1] + 0.07 * image.data[pos + 2]) * image.data[pos + 3] / 255) < threshold ? 1 : 0);
+          break;
+      }
+    }
+
+    rows.push(cols);
+  }
+
+  return {
+    height: image.height,
+    width: image.width,
+    data: rows,
+  };
+}
+
+function convertToLabelBuffer(
+  pixelMatrix: PixelMatrix,
+  tapeWidth: number,
+  highResolution: boolean,
+) {
+  const data = [
+    // Invalidate
+    Buffer.alloc(400),
+
+    // Initialize
+    Buffer.from([0x1B, 0x40]),
+
+    // Switch to Raster Mode
+    Buffer.from([0x1B, 0x69, 0x61, 0x01]),
+
+    // Print information command — Sets tape width
+    // 0x02 Media type
+    // 0x04 Media width
+    // 0x08 Media length
+    // 0x40 Priority given to print quality(Not used)
+    // 0x80 Printer recovery always on
+    Buffer.from([0x1B, 0x69, 0x7A, 0x84, 0x00, tapeWidth, 0x00, 0xAA, 0x02, 0x00, 0x00, 0x00, 0x00]),
+
+    // Various mode settings - Auto Cut
+    Buffer.from([0x1B, 0x69, 0x4D, 0x40]),
+
+    // Cut per label setting - 0x01 -> each label
+    Buffer.from([0x1B, 0x69, 0x41, 0x01]),
+
+    // Advanced Setting Mode
+    // 2bit half-cut, 3bit no chain printing, 4bit special tape, 6bit high-res, 7bit mirror
+    Buffer.from([0x1B, 0x69, 0x4B, 0x0C | ((highResolution ? 1 : 0) << 6)]),
+
+    // Margin feed amount - 28 dots (2mm)
+    Buffer.from([0x1B, 0x69, 0x64, 14 * (highResolution ? 2 : 1), 0x00]),
+
+    // Enable TIFF - Compression
+    // ..on P750W seems like has an issue with 0x1A command and doesn't cut or return back to idle mode
+    // unless TIFF compression is enabled.
+    Buffer.from([0x4D, 0x02]),
+  ];
+
+  // Iterate over Image matrix
+  for (let x = 0; x < pixelMatrix.width; x++) {
+    // Each row has 3 bytes for command + 1 TIFF byte + 16 bytes for raster
+    let rowBuffer = Buffer.alloc(20);
+
+    // Raster Row Command
+    rowBuffer[0] = 0x47;
+    rowBuffer[1] = 0x11; // 17
+    rowBuffer[3] = 0x0F;
+
+    let margin = (tapeWidth === 12) ? 21 : 0;
+    for (let y = 0; y < pixelMatrix.height; y++) {
+      if (pixelMatrix.data[y][x] == 1) {
+        let byteNum = (Math.floor((y + margin) / 8) + 4); // 3 for command + 1 for TIFF compression length byte
+        let bitOffset = (y + margin) % 8;
+        rowBuffer[byteNum] |= (1 << 7 - bitOffset);
+      }
+    }
+    data.push(rowBuffer);
+  }
+
+  // Debugging
+  // data.map(row => console.log(row));
+
+  // Send print + cut command
+  data.push(Buffer.from([0x1A]));
+  return Buffer.concat(data);
+}
+
+function resolveOptions(options: PrintOptions = {}): InternalOptions {
+  return {
+    blackwhiteThreshold: 128,
+    highResolution: false,
+    tapeWidth: 12 as const,
+    ...options,
+  };
+}
+
+async function print(
+  printerUrl: string,
+  input: { type: 'file', data: string } | { type: 'buffer', data: Buffer },
+  options: PrintOptions,
+) {
+  const resolvedOptions = resolveOptions(options);
+  const bufferToBePrinted = await convertToLabelBuffer(
+    convertToBlackAndWhiteMatrix(
+      input.type === 'buffer' ? await parse(input.data) : await parseFile(input.data),
+      resolvedOptions.blackwhiteThreshold,
+    ),
+    resolvedOptions.tapeWidth,
+    resolvedOptions.highResolution,
+  );
+
   const printer = ipp.Printer(printerUrl);
   const execute = util.promisify(printer.execute.bind(printer));
 
@@ -38,13 +191,12 @@ export async function doPrint(printerUrl, bufferToBePrinted) {
   console.log('Print data sent.');
 
   if (res.statusCode === 'successful-ok'
-    || res.statusCode === 'successful-ok-ignored-or-substituted-attributes') 
-  {
+    || res.statusCode === 'successful-ok-ignored-or-substituted-attributes') {
     console.log('Printing successful.');
     const jobId = res['job-attributes-tag']['job-id'];
     let tries = 0;
     let job;
-    
+
     await wait(500);
     while (tries <= 50) {
       tries += 1;
@@ -53,7 +205,7 @@ export async function doPrint(printerUrl, bufferToBePrinted) {
         'Get-Job-Attributes',
         { 'operation-attributes-tag': { 'job-id': jobId } },
       );
-      
+
       if (job && job['job-attributes-tag']['job-state'] === 'completed') {
         return job;
       }
@@ -74,184 +226,12 @@ export async function doPrint(printerUrl, bufferToBePrinted) {
     console.log(res);
     throw new Error('Error sending job to printer!');
   }
+}
+
+export const printPngFile = async function (printerUrl: string, filename: string, options: PrintOptions = {}) {
+  return print(printerUrl, { type: 'file', data: filename }, options);
 };
 
-function convertToBlackAndWhiteMatrixImage(image, options) {
-  // convert image to matrix of pixels:
-  let rows:number[][] = [];
-
-  for (let y = 0; y < image.height; y++) {
-    let cols:number[] = [];
-    for (let x = 0; x < image.width; x++) {
-      let pos = x + image.width*y;
-
-
-      pos = pos * image.channels;
-      let pixel = 0; // white = 0, black = 1
-
-      let threshold = options.blackwhiteThreshold;
-      let gray;
-
-      // 1 channel : grayscale
-      // 2 channels: grayscale + alpha
-      // 3 channels: RGB
-      // 4 channels: RGBA
-      switch(image.channels) {
-        case 1:
-          if (image.data[pos] < threshold) pixel = 1;
-          break;
-
-        case 2:
-          gray = image.data[pos] *  image.data[pos+1]/255;
-          if (gray < threshold) pixel = 1;
-          break;
-
-        case 3:
-          gray = 0.21 * image.data[pos] + 0.72 * image.data[pos+1] + 0.07 * image.data[pos+2];
-          if (gray < threshold) pixel = 1;
-          break;
-
-        case 4:
-          gray = (0.21 * image.data[pos] + 0.72 * image.data[pos+1] + 0.07 * image.data[pos+2]) * image.data[pos+3] / 255;
-          if (gray < threshold) pixel = 1;
-          break;
-      }
-
-      cols.push(pixel);
-    }
-    rows.push(cols);
-  }
-
-  return {
-    height: image.height,
-    width: image.width,
-    data: rows
-  };
-}
-
-function rotateMatrixImage({ width, height, data }) {
-  let rows:any[][] = [];
-  for (let x = 0; x < width; x++) {
-    let cols:any[] = [];
-    for (let y = height - 1; y >= 0; y--) {
-      cols.push(data[y][x]);
-    }
-    rows.push(cols);
-  }
-
-  return {
-    height: width,
-    width: height,
-    data: rows
-  };
-}
-
-function convertImageToDotlabel({
-  image, highResolution, tapeWidth,
-} = {
-  image: {
-    height: 0, width: 0, data: Array(Array()),
-  },
-  highResolution: false,
-  tapeWidth: 12,
-}) {
-
-  let data = [
-    // Invalidate
-    Buffer.alloc(400),
-
-    // Initialize
-    Buffer.from([0x1B, 0x40]),
-    
-    // Switch to Raster Mode
-    Buffer.from([0x1B, 0x69, 0x61, 0x01]),
-
-    // Print information command — Sets tape width
-    // 0x02 Media type
-    // 0x04 Media width
-    // 0x08 Media length
-    // 0x40 Priority given to print quality(Not used)
-    // 0x80 Printer recovery always on
-    Buffer.from([0x1B, 0x69, 0x7A, 0x84, 0x00, tapeWidth, 0x00, 0xAA, 0x02, 0x00, 0x00, 0x00, 0x00]),
-
-    // Various mode settings - Auto Cut
-    Buffer.from([0x1B, 0x69, 0x4D, 0x40]),
-
-    // Cut per label setting - 0x01 -> each label
-    Buffer.from([0x1B, 0x69, 0x41, 0x01]),
-
-    // Advanced Setting Mode
-    // 2bit half-cut, 3bit no chain printing, 4bit special tape, 6bit high-res, 7bit mirror
-    Buffer.from([0x1B, 0x69, 0x4B, 0x0C | ((highResolution ? 1 : 0) << 6) ]),
-
-    // Margin feed amount - 28 dots (2mm)
-    Buffer.from([0x1B, 0x69, 0x64, 14 * (highResolution ? 2 : 1), 0x00]),
-
-    // Enable TIFF - Compression
-    // ..on P750W seems like has an issue with 0x1A command and doesn't cut or return back to idle mode
-    // unless TIFF compression is enabled.
-    Buffer.from([0x4D, 0x02]),
-  ];
-
-  // Iterate over Image matrix
-  for (let x = 0; x < image.width; x++) {
-    // Each row has 3 bytes for command + 1 TIFF byte + 16 bytes for raster
-    let rowBuffer = Buffer.alloc(20);
-
-    // Raster Row Command 
-    rowBuffer[0] = 0x47;
-    rowBuffer[1] = 0x11; // 17
-    rowBuffer[3] = 0x0F; 
-
-    let margin = (tapeWidth === 12) ? 21 : 0;
-    for (let y = 0; y < image.height; y++) {
-      if (image.data[y][x] == 1) {
-        let byteNum = (Math.floor((y + margin) / 8) + 4); // 3 for command + 1 for TIFF compression length byte
-        let bitOffset = (y + margin) % 8;
-        rowBuffer[byteNum] |= (1 << 7 - bitOffset);
-      }
-    }
-    data.push(rowBuffer);
-  }
-
-  // Debugging
-  // data.map(row => console.log(row));
-
-  // Send print + cut command
-  data.push(Buffer.from([0x1A]));
-  return Buffer.concat(data);
-}
-
-async function convert(image, options = {
-  highResolution: false,
-  tapeWidth: 12,
-  blackwhiteThreshold: 128,
-}) {
-  let defaultOptions = {
-    tapeWidth: 12,
-    blackwhiteThreshold: 128
-  };
-
-  // Convert to 'label image' or something that the label printer understands
-  return convertImageToDotlabel({
-    image: convertToBlackAndWhiteMatrixImage(image, {
-      ...defaultOptions,
-      ...options,
-    }),
-    ...options,
-  });
-}
-
-export const printPngFile = async function(printerUrl, filename, options) {
-  let parseFile = util.promisify(pngparse.parseFile);
-  let img = await parseFile(filename);
-  
-  let printData = await convert(img, options);
-  return await doPrint(printerUrl, printData);
-};
-
-export const printBuffer = async function(printerUrl, buffer, options) {
-  let parse = util.promisify(pngparse.parse);
-  let printData = await convert(await parse(buffer), options);
-  return await doPrint(printerUrl, printData);
+export const printBuffer = async function (printerUrl: string, buffer: Buffer, options: PrintOptions = {}) {
+  return print(printerUrl, { type: 'buffer', data: buffer }, options);
 };
